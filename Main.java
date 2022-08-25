82f6e789ddbfcdb1d8e8fe2c2dfaec1b42c360de
AJ <63066039+ajworks13@users.noreply.github.com>
2022-08-25 12:25:50 -0400

    Create Main.java
    4 videos completed.

---
+++ Main.java
@@ -0,0 +1,80 @@
+class Main {
+  public static void main(String[] args) {
+
+		
+
+		// Primative types ---------------------------------------
+		/*
+		boolean = true or false; 1 byte of memory
+		byte -128 to 128; 1 byte of memory
+		short -2 15th power to 2 15th power -1 
+		char unicode '\u0000' to '\uffff' 16 bits (2 bytes of memory)
+		int -2 31power to 2 31power -1 32 bits (4 bytes of memory)
+		float 32 bite 754 floating point. o.15f or 1.25e4f (8 bytes of memory)
+		double 64 bit 754 floating point (8 bytes of memory)
+
+		Reference Variable: String
+		String name = "Bob";
+
+		Array: Collection of primative values. Fixed size data type.
+		int[] intArray = new int[10];
+		int[] intArray = {2,4,6};
+		System.out.println(intArray[2]) // 6
+
+		Multidimension Array:
+		int [][] myArray
+
+
+		instance variable: declared in the class scope.
+		static variable: shared across all instances of a class. 
+		local variables: declared inside a method or nested scope.
+
+
+
+		// Operators: ------------------------------------------
+    
+			Order of operations by highest presidence:
+				Parenthesis, Array indices, dot: () []
+				Postfix: ++  -- // int a = 0 // a++ is now 1
+				Multiplicative: *  /  %
+					* - Multiplication.. / - Division.. % - Modulus..
+				Additive: +   -
+				Shift: <<    >>    >>>
+				Relational: <   >   <=   >=   instanceof
+				Equality: ==   !=
+				Bitwise: &   ^   |
+					&: bitwise AND
+					^: bitwise XOR
+					|: bitwise OR
+				Logical: &&   ||
+				Ternary: expr ? expr : expr
+							(a < 4) ? (b = 1) : (b = 2);
+				Assignment:
+						=  +=  -=  *=  /=  %=  &=  ^=  |=  <<=  >>=  >>>=
+
+		*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	}
+}
